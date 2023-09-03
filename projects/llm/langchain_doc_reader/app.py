b7e7d39549189ce1ec8c4ca96dfc556e3e0a5fa6
blancas-armando <blancas.armandoc@gmail.com>
2023-09-02 22:30:39 -0700

    hard coding to run manually for now

--- projects/llm/langchain_doc_reader/app.py
+++ projects/llm/langchain_doc_reader/app.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 import openai
 import streamlit as st
 from PIL import Image
@@ -14,19 +17,13 @@ from langchain.agents.agent_toolkits import (
 
 
 def main():
-    # load_dotenv() == st.secrets['OPENAI_API_KEY']
-    # openai.api_key = os.getenv('OPENAI_API_KEY')
-    with st.sidebar:
-        st.image(Image.open(r'jpmorganchase_logo.png'))
-        st.write('This is a personal project and does not reflect JPMorgan Chase & Co.')
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    openai.api_key = openai_api_key
     llm = OpenAI(temperature=0.1, verbose=True)
     embeddings = OpenAIEmbeddings()
 
-    loader = PyPDFLoader(r'jpmc_annualreport_2022.pdf')
+    loader = PyPDFLoader(r'projects\llm\langchain_doc_reader\jpmc_annualreport_2022.pdf')
 
     pages = loader.load_and_split()
     store = Chroma.from_documents(pages, embeddings, collection_name='jpmc_annualreport')
@@ -44,6 +41,10 @@ def main():
         verbose=True
     )
     
+    with st.sidebar:
+        st.image(Image.open(r'projects\llm\langchain_doc_reader\jpmorganchase_logo.png'))
+        st.write('This is a personal project and does not reflect JPMorgan Chase & Co.')
+
     st.title(':bookmark_tabs: 2022 Annual Report Reader')
     prompt = st.text_input('Input your prompt here')
 
