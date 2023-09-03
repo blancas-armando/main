542818b1a05aa7977773c3f42c72b5ee6fbbafc9
blancas-armando <blancas.armandoc@gmail.com>
2023-09-02 18:05:34 -0700

    adding sequential chain

--- projects/llm/app.py
+++ projects/llm/app.py
@@ -4,7 +4,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -12,20 +12,25 @@ st.title('Job Interview Prep')
 prompt = st.text_input('Input the role you are applying for here')
 
 # Prompt Template
-title_template = PromptTemplate(
+interview_question_template = PromptTemplate(
     input_variables= ['role'],
-    template='Write me interview questions for a {role} position'
+    template='Write me the most important interview questions for a {role} position'
+)
+
+interview_prep_template = PromptTemplate(
+    input_variables= ['role'],
+    template='Write me an interview prep guideline for a {role} position'
 )
 
 # LLMs
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(
-    llm=llm,
-    prompt=title_template,
-    verbose=True
-)
+interview_question_chain = LLMChain(llm=llm, prompt=interview_question_template, verbose=True)
+interview_prep_chain = LLMChain(llm=llm, prompt=interview_prep_template, verbose=True)
+sequential_chain = SimpleSequentialChain(chains=[interview_question_chain, interview_prep_chain], verbose=True)
+
+
 # Show to screen if prompt is provided
 if prompt:
-    response = title_chain.run(prompt)
+    response = sequential_chain.run(role=prompt)
     st.write(response)
 
