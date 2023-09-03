b96175c674bf708388866729fc9b9c20eed4f96e
blancas-armando <blancas.armandoc@gmail.com>
2023-09-02 20:03:10 -0700

    final adjustments for init version of app

--- projects/llm/langchain_doc_reader/app.py
+++ projects/llm/langchain_doc_reader/app.py
@@ -1,52 +1,55 @@
+# Import os to set API key
 import os
 from dotenv import load_dotenv
 
 import openai
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
-
 import streamlit as st
 
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
-
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
     VectorStoreInfo
 )
 
-load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
-llm = OpenAI(temperature=0.3, verbose=True)
-embeddings = OpenAIEmbeddings()
+def main():
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
-loader = PyPDFLoader('frb_earnings_release_q4_2022.pdf')
-pages = loader.load_and_split()
-store = Chroma.from_documents(pages, embeddings, collection_name='earnings_release')
+    llm = OpenAI(temperature=0.1, verbose=True)
+    embeddings = OpenAIEmbeddings()
 
-vectorstore_info = VectorStoreInfo(
-    name='frb_earnings_release_q4_2022',
-    description='frb earnings report for q4 2022',
-    vectorstore=store
-)
+    loader = PyPDFLoader(r'projects\llm\langchain_doc_reader\jpmc_annualreport_2022.pdf')
 
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    pages = loader.load_and_split()
+    store = Chroma.from_documents(pages, embeddings, collection_name='jpmc_annualreport')
 
-agent_executor = create_vectorstore_agent(
-    llm=llm,
-    tookit=toolkit,
-    verbose=True
-)
+    vectorstore_info = VectorStoreInfo(
+        name="annual_report_2022",
+        description="JPMC annual report 2022 PDF",
+        vectorstore=store
+    )
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 
-st.title('Page Title')
-prompt=st.text_input('Input your prompt here')
+    agent_executor = create_vectorstore_agent(
+        llm=llm,
+        toolkit=toolkit,
+        verbose=True
+    )
+    st.title('Bank Annual Report Reader Example')
+    prompt = st.text_input('Input your question here')
 
-if prompt:
-    response = agent_executor.run(prompt)
-    st.write(response)
+    if prompt:
+        response = agent_executor.run(prompt)
+        st.write(response)
+        with st.expander('Document Similarity Search'):
+            search = store.similarity_search_with_score(prompt)  
+            st.write(search[0][0].page_content)
 
-    with st.expander(''):
-        search = store.similarity_search_with_score(prompt)
-        st.write(search[0][0].page_content)
+
+if __name__ == '__main__':
+    main()
