30159d65de9f6832106833eb78742b448325dfd6
blancas-armando <blancas.armandoc@gmail.com>
2023-09-02 18:31:24 -0700

    fix issue

--- projects/llm/app.py
+++ projects/llm/app.py
@@ -1,6 +1,6 @@
 # Bring in deps
 import os 
-from apikey import apikey 
+from dotenv import load_dotenv
 
 import streamlit as st 
 from langchain.llms import OpenAI
@@ -9,7 +9,8 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+load_dotenv()
+os.getenv('apikey')
 
 # App framework
 st.title('🦜🔗 YouTube GPT Creator')
