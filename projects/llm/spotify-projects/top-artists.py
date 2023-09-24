88298a33963b3380d111a723dbb1bf7120dbf79e
blancas-armando <blancasarmando2@gmail.com>
2023-09-24 12:12:42 -0400

    feat: making headway on st app

--- projects/llm/spotify-projects/top-artists.py
+++ projects/llm/spotify-projects/top-artists.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 import json
 
 import pandas as pd
+import numpy as np
+import urllib.request
+import cv2
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
+from PIL import Image
 
 
 def main():
@@ -18,29 +23,34 @@ def main():
                                                    redirect_uri='https://flippingbook.com/404',
                                                    scope=scope))
 
-    # Current User Display Nmae
+    #TODO: Maybe we should create a df for each user and store info in columns?
+    # Current User Display Nmae and Image
     print('User Display Name')
     print('--------------------')
-    print(sp.current_user()['display_name'])
+    display_name = sp.current_user()['display_name']
     print('--------------------')
     
-    # Current User Image
     print('User Image')
     print('--------------------')
     print(sp.current_user()['images'][0]['url'])
+    #TODO Need to find better way to convert and store user image; this works for now
+    image = urllib.request.urlretrieve(url=sp.current_user()['images'][0]['url'], filename='spotify_user_image.png')
     print('--------------------')
 
 
     # Short Term Top Artists
     print('Recent Top Artists')
+    st.text('Recent Top Artists')
     print('--------------------')
     user_top_artists_results = sp.current_user_top_artists(time_range='short_term', limit=10)
     for i, item in enumerate(user_top_artists_results['items']):
+        #TODO Let's append these to a list and display list?
         print(i, item['name'])
     print('--------------------')
 
     # Short Term Top Tracks
     print('Recent Top Tracks')
+    st.text('Recent Top Tracks')
     print('------------------')
 
     user_top_tracks_results = sp.current_user_top_tracks(time_range='short_term', limit=10)   
@@ -48,6 +58,10 @@ def main():
         print(i, item['name'] +  ' by ' + item['album']['artists'][0]['name'] + ' (' + item['uri'] + ') ')
     print('--------------------')
 
+    with st.sidebar:
+        st.title('Spotify User Analytics v1')
+        st.image(Image.open('spotify_user_image.png'))
+        st.text(f'{display_name}')
 
 if __name__ == '__main__':
     main()
