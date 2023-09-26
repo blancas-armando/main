e5a3f25ee4ae2893dcf99cf54b0bcf6c55958c4b
blancas-armando <blancasarmando2@gmail.com>
2023-09-25 20:24:37 -0700

    feat: changing scope of project

--- projects/llm/spotify-projects/user_top_info.py
+++ projects/llm/spotify-projects/user_top_info.py
@@ -23,8 +23,7 @@ def main():
                                                    redirect_uri='https://flippingbook.com/404',
                                                    scope=scope))
 
-    #TODO: Maybe we should create a df for each user and store info in columns?
-    # Current User Display Nmae and Image
+    # Current User Display Name and Image
     print('User Display Name')
     print('--------------------')
     display_name = sp.current_user()['display_name']
@@ -37,6 +36,10 @@ def main():
     image = urllib.request.urlretrieve(url=sp.current_user()['images'][0]['url'], filename='spotify_user_image.png')
     print('--------------------')
 
+    # with st.sidebar:
+    #     st.title('Spotify User Analytics v1')
+    #     st.image(Image.open('spotify_user_image.png'))
+    #     st.text(f'{display_name}')
 
     # Short Term Top Artists
     print('Recent Top Artists')
@@ -46,22 +49,27 @@ def main():
     for i, item in enumerate(user_top_artists_results['items']):
         #TODO Let's append these to a list and display list?
         print(i, item['name'])
+        for i, genre in enumerate(item['genres']):
+            print('Genre ' + str(i) + ': ' + genre)
+        print('--------------------')
     print('--------------------')
 
     # Short Term Top Tracks
     print('Recent Top Tracks')
     st.text('Recent Top Tracks')
     print('------------------')
-
-    user_top_tracks_results = sp.current_user_top_tracks(time_range='short_term', limit=10)   
+    user_top_tracks_results = sp.current_user_top_tracks(time_range='short_term', limit=10)
     for i, item, in enumerate(user_top_tracks_results['items']):
-        print(i, item['name'] +  ' by ' + item['album']['artists'][0]['name'] + ' (' + item['uri'] + ') ')
+        print(i, item['name'] +  ' by ' + item['album']['artists'][0]['name'])
+        track_features = sp.audio_features(tracks=item['uri'])
+        for track in track_features:
+            print('Danceability: ' + str(round(track['danceability'], 2)*100) + '%')
+            print('Energy percentage: ' + str(round(track['energy'], 2)*100) + '%')
+            print('Loudness: ' + str(track['loudness']))
+            print('Tempo: ' + str(track['tempo']) + ' bpm')
+            print('--------------------')
     print('--------------------')
 
-    with st.sidebar:
-        st.title('Spotify User Analytics v1')
-        st.image(Image.open('spotify_user_image.png'))
-        st.text(f'{display_name}')
 
 if __name__ == '__main__':
     main()
